0d4ad36f9c5d4b3ba5736788f1ca38363e0d45bf
Masashi Uemura <masashi@MasashiMB.local>
2018-05-24 17:23:13 +0900

    docomoAPIの画像認識を実装

--- Landmarker/explainingViewController.swift
+++ Landmarker/explainingViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 import ImageRecognition
 import Alamofire
+import SwiftyJSON
 
 class explainingViewController: UIViewController {
 
@@ -21,7 +22,7 @@ class explainingViewController: UIViewController {
         targetImage.image = originalImage
         
         // post
-        //
+        recoginitionLandmarkImage(image: originalImage!)
         
         titleLabel.text = "Landmarkの名前"
         documentLabel.text = "Landmarkの説明"
@@ -39,42 +40,69 @@ class explainingViewController: UIViewController {
     
     var originalImage : UIImage?
     
+    struct response_docomoAPI: Codable {
+        let candidates: (candidate)
+        let jobId: String
+    }
     
+    struct candidate: Codable{
+        let score: String
+        let tag: String
+    }
     
-    
-    
-    func recoginitionLandmarkImage(image: UIImage) -> String{
+    func recoginitionLandmarkImage(image: UIImage){
         let headers: HTTPHeaders = ["Content-Type": "multipart/form-data"]
-        let parameters: Parameters = ["APIKEY": "6d667135466d7a414c2e6c477458724d5353764d61315170584647726d6647757639416854427639527a42", "modelName":"landmark"]
+        //let parameters: Parameters = ["modelName":"landmark"]
+        
+        //var name_landmark: String?
         
         Alamofire.upload(
-            "https://api.apigw.smt.docomo.ne.jp/imageRecognition/v1/concept/classify",
-            method: .post,
-            headers: headers,
-            multipartFormData: { multipartFormData in
-                multipartFormData.append(UIImageJPEGRepresentation(image), withName: "image", mimeType: "image/jpeg")
+            multipartFormData :{ (MultipartFormData) in
+                // image
+                MultipartFormData.append(UIImageJPEGRepresentation(image, 1)!, withName: "image", fileName: "sample.jpeg", mimeType: "image/jpeg")
                 
+                // param
+                //MultipartFormData.append(data: "landmark", withName: "modelName")
+                MultipartFormData.append("landmark".data(using: .utf8)!, withName: "modelName")
+                
+                /**
                 for (key,value) in parameters{
-                    multipartFormData.append(data: value.dataUsingEncoding(NSUTF8StringEncoding)!, name: key)
-                }
-            },
+                    MultipartFormData.append(data: (value as AnyObject).dataUsingEncoding(String.Encoding.utf8.rawValue)!, name: key)
+                } **/
+                //dump(MultipartFormData)
+        },
+            to: "https://api.apigw.smt.docomo.ne.jp/imageRecognition/v1/concept/classify/?APIKEY=6d667135466d7a414c2e6c477458724d5353764d61315170584647726d6647757639416854427639527a42",
+            //method: .POST,
+            headers: headers,
             encodingCompletion: { encodingResult in
                 switch encodingResult {
                 case .success(let upload, _, _):
                     upload.responseJSON { response in
                         // 成功
                         let responseData = response
-                        print(responseData ?? "成功")
+                        print("!success!")
+                        print(responseData)
+                        
+                        print("----- json parse -----")
+                        let json:JSON = JSON(responseData.result.value ?? kill)
+                        print(json["candidates"][0]["tag"].stringValue)
+                        print(type(of: json["candidates"][0]["tag"].stringValue))
+                        
+                        self.titleLabel.text = json["candidates"][0]["tag"].stringValue
+                        
+                        //name_landmark = json["candidates"][0]["tag"].stringValue
                     }
                 case .failure(let encodingError):
                     // 失敗
+                    print("error")
                     print(encodingError)
                 }
             }
         )
         
+        //return name_landmark!
+        
     }
-
 
     /*
     // MARK: - Navigation
--- Pods/Target Support Files/Pods-Landmarker/Pods-Landmarker-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Landmarker : NSObject
-@end
-@implementation PodsDummy_Pods_Landmarker
-@end
